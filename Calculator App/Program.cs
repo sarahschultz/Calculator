9da43a619ebc86560e00df12ca8a0f877317ee3d
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-08-01 16:44:01 -0500

    committing 4:43pm

--- Calculator App/Program.cs
+++ Calculator App/Program.cs
@@ -4,7 +4,9 @@ namespace MyCalculatorApp
 {
     static void Main(string[] args)
     {
-        double num1;
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
 
         Console.WriteLine("-------");
         Console.WriteLine("Calculator App");
