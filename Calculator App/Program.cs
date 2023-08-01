4783d5635727213b8c7817f13a6b39d880c21773
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-08-01 17:00:49 -0500

    Using code giraffe

--- Calculator App/Program.cs
+++ Calculator App/Program.cs
@@ -2,59 +2,21 @@
 
 namespace MyCalculatorApp
 {
-    static void Main(string[] args)
-
+    class Program
     {
-        do { 
-            double num1 = 0;
-            double num2 = 0;
-            double result = 0;
-
-            Console.WriteLine("-------");
-            Console.WriteLine("Calculator App");
-            Console.WriteLine("-------");
-
-        Console.Write("Enter number 1: ");
-        num1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter number 2: ");
-        num2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Enter an option: ");
-        Console.WriteLine("\t+ : Add");
-        Console.WriteLine("\t- : Subtract");
-        Console.WriteLine("\t* : Multiply");
-        Console.WriteLine("\t/ : Divide");
-        Console.Write("Enter an option: ");
-
-
-        switch (Console.ReadLine())
+        static void Main(string[] args)
         {
-            case "+":
-                result = num1 + num2;
-                Console.WriteLine($"Your result: {num1} + {num2} = " + result);
-                break;
-            case "-":
-                result = num1 - num2;
-                Console.WriteLine($"Your result: {num1} - {num2} = " + result);
-                break;
-            case "*":
-                result = num1 * num2;
-                Console.WriteLine($"Your result: {num1} * {num2} = " + result);
-                break;
-            case "/":
-                result = num1 / num2;
-                Console.WriteLine($"Your result: {num1} / {num2} = " + result);
-                break;
-            default:
-                Console.WriteLine("That was not a valid option");
-                break;
-        }
-        Console.Write("Would you like to continue? (Y = yes, N = No): ");
-    } while (Console.ReadLine().ToUpper() == "Y");
+            Console.Write("Enter a Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Bye!");
+            Console.Write("Enter another Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+            Console.WriteLine(num1 + num2);
+
+      
+
+            Console.ReadLine();
         }
+    }
 }
