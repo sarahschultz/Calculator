c5cf42f1b7d6b50f4c04d96e66718fd7a43754a6
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-08-01 16:52:05 -0500

    updated code base

--- Calculator App/Program.cs
+++ Calculator App/Program.cs
@@ -3,7 +3,9 @@
 namespace MyCalculatorApp
 {
     static void Main(string[] args)
+
     {
+        do { 
             double num1 = 0;
             double num2 = 0;
             double result = 0;
@@ -11,6 +13,47 @@ namespace MyCalculatorApp
             Console.WriteLine("-------");
             Console.WriteLine("Calculator App");
             Console.WriteLine("-------");
+
+        Console.Write("Enter number 1: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter number 2: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an option: ");
+        Console.WriteLine("\t+ : Add");
+        Console.WriteLine("\t- : Subtract");
+        Console.WriteLine("\t* : Multiply");
+        Console.WriteLine("\t/ : Divide");
+        Console.Write("Enter an option: ");
+
+
+        switch (Console.ReadLine())
+        {
+            case "+":
+                result = num1 + num2;
+                Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                break;
+            case "*":
+                result = num1 * num2;
+                Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                break;
+            case "/":
+                result = num1 / num2;
+                Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                break;
+            default:
+                Console.WriteLine("That was not a valid option");
+                break;
+        }
+        Console.Write("Would you like to continue? (Y = yes, N = No): ");
+    } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Bye!");
 
             Console.ReadKey();
         }
